abb662da0a112b5fe944f227ed31b7d97ea6d3d3
Yolanda <1145728724@qq.com>
2020-07-08 14:10:55 +0800

    set的另一种用法

--- mytest/src/mytest/CalendarTest.java
+++ mytest/src/mytest/CalendarTest.java
@@ -9,8 +9,11 @@ public class CalendarTest {
 		//set(year,month,date) 把 Calendar 对象的年月日设置为指定时间
 		c1.set(2020,7,6);
 		//get(filed) 获取 Calendar 对象的值
-		System.out.println("Calendar 时间：" + c1.get(Calendar.YEAR) + " " + c1.get(Calendar.MONTH) + " " + c1.get(Calendar.DATE));
-		//set(filed,value) 如果单独设置某个日期对象（年月日）的值，可用这种方法
-		
+		System.out.println("Calendar 时间：" + c1.get(Calendar.YEAR) + "-" + c1.get(Calendar.MONTH) + "-" + c1.get(Calendar.DATE) + " " + c1.get(Calendar.HOUR) + ":" + c1.get(Calendar.MINUTE) + ":" + c1.get(Calendar.SECOND));
+		//set(filed,value) 如果单独设置某个日期对象的值，可用这种方法
+		c1.set(Calendar.DATE,8);
+		System.out.println(c1.get(Calendar.DATE));
+		c1.set(Calendar.YEAR,2021);
+		System.out.println(c1.get(Calendar.YEAR));
 	}
 }
