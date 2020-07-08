aa692af0fa1f0c1cd01ce9e0fe96a868cfd7440a
Yolanda <1145728724@qq.com>
2020-07-08 16:57:24 +0800

    正则{n}{n,}{n,m}

--- mytest/src/mytest/RegexTest.java
+++ mytest/src/mytest/RegexTest.java
@@ -17,5 +17,27 @@ public class RegexTest {
 		boolean m = Pattern.matches(pat,str);
 		System.out.println("字符串是否为'test'：" + m);
 		
+		//{n} n 是非负整数。表示{n} 之前的字符正好匹配 n 次。
+		String f = "food";
+		String g = "group";
+		String p1 = ".*o{2}.*";//字符 o 出现两次 
+		boolean mf = Pattern.matches(p1,f);
+		System.out.println("字符串'" + f + "'是否含有两个'o'：" + mf);
+		System.out.println("字符串'" + g + "'是否含有两个'o'：" + Pattern.matches(p1,g));
+		//{n,} n 是非负整数。表示{n,} 之前的字符至少匹配 n 次。
+		String s = "start";
+		String p2 = ".*o{1}.*";
+		System.out.println("字符串'" + f + "'是否至少含有一个'o'：" + Pattern.matches(p2,f));
+		System.out.println("字符串'" + g + "'是否至少含有一个'o'：" + Pattern.matches(p2,g));
+		System.out.println("字符串'" + s + "'是否至少含有一个'o'：" + Pattern.matches(p2,s));
+		//{n,m} n 和 m 是非负整数,n <= m,匹配之前的字符至少 n 次，至多 m 次
+		String p3 = ".*o{2,4}.*";
+		System.out.println("字符串'" + f + "'是否含有两个到四个'o'：" + Pattern.matches(p3,f));
+		System.out.println("字符串'" + g + "'是否含有两个到四个'o'：" + Pattern.matches(p3,g));
+		System.out.println("字符串'" + s + "'是否含有两个到四个'o'：" + Pattern.matches(p3,s));
+		String p4 = ".*o{0,1}.*";
+		System.out.println("字符串'" + f + "'是否不含有或者只含有一个'o'：" + Pattern.matches(p4,f));//匹配了第一个o
+		System.out.println("字符串'" + g + "'是否不含有或者只含有一个'o'：" + Pattern.matches(p4,g));
+		System.out.println("字符串'" + s + "'是否不含有或者只含有一个'o'：" + Pattern.matches(p4,s));
 	}
 }
